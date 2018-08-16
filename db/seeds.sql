aa5de14330a4837875136af5435da101d0ec130f
Tarante <37225235+riegit@users.noreply.github.com>
2018-08-15 23:51:43 -0400

    commiting all files

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Plain Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Chicken Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Spicy Ham and Egg burger');
